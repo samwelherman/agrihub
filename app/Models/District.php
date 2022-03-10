e2f59962061a2f9844afdad05667700da590bd19
samwelherman <samwelherman85@gmail.com>
2022-03-09 22:07:27 -0800

    =models

---
+++ app/Models/District.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class District extends Model
+{
+    //
+    
+    public function region()
+    {
+        return $this->belongsTo('App\Models\Region');
+    }
+}
