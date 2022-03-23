f8a858613aa320bbeeb10ed4b4781f5d22f6c6d2
said minda <52763565+saidiminda@users.noreply.github.com>
2022-03-23 09:12:09 +0300

    fixing access database error

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInitf23cf27139e659cb4bf3f1cf81d9fdf6
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitf23cf27139e659cb4bf3f1cf81d9fdf6', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf23cf27139e659cb4bf3f1cf81d9fdf6', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
